e00279b002e711d7f6adf6c5cd93fc800b19b8b2
francescompassaro <francescompassaro@gmail.com>
2025-02-17 17:25:01 +0100

    Primo commit

---
+++ src/main/java/com/LegaPauperCapua/LegaPauperCapua/LegaPauperCapuaApplication.java
@@ -0,0 +1,13 @@
+package com.LegaPauperCapua.LegaPauperCapua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegaPauperCapuaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegaPauperCapuaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/LegaPauperCapua/LegaPauperCapua/LegaPauperCapuaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LegaPauperCapua.LegaPauperCapua;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegaPauperCapuaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
